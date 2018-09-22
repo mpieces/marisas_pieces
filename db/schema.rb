336c0544c7af3e6b69764f24cc0e66fb80709b1a
Marisa Ramon <mer0522@gmail.com>
2018-09-21 21:25:35 -0700

    Create sessions, log in ability, and admin controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180922024922) do
+ActiveRecord::Schema.define(version: 20180922033506) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -48,6 +48,13 @@ ActiveRecord::Schema.define(version: 20180922024922) do
     t.string "dimensions"
     t.decimal "price", precision: 8, scale: 2
     t.integer "yop"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
