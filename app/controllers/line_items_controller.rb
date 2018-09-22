d528f8f0b280d0f9cc94626965a4649dddcf79e8
Marisa Ramon <mer0522@gmail.com>
2018-09-21 18:20:46 -0700

    Create an ajax-based cart, fix jquery bug

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -31,7 +31,8 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart }
+        format.html { redirect_to store_index_url }
+        format.js
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
@@ -59,7 +60,7 @@ class LineItemsController < ApplicationController
   def destroy
     @line_item.destroy
     respond_to do |format|
-      format.html { redirect_to line_items_url, notice: 'Line item was successfully destroyed.' }
+      format.html { redirect_to @line_item.cart, notice: 'Item was successfully removed from cart.' }
       format.json { head :no_content }
     end
   end
