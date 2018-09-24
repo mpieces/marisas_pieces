6eab34cb9799e67cd8f238d7f5153efe198aa654
Marisa Ramon <mer0522@gmail.com>
2018-09-23 19:26:32 -0700

    Add CSS styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.delete_all
+
 Product.create!(image_url: '1.jpg',
 maker: 'Fenton',
 title: '2-tier tidbit tray',
@@ -25,4 +28,12 @@ Product.create!(image_url: '3.jpg',
   pattern: 'peacock tail',
   color: 'green',
   price: 75.00,
+  yop: 1940)
+
+Product.create!(image_url: '4.jpg',
+  maker: 'Fenton',
+  title: 'carnival glass punch bowl',
+  pattern: 'orange tree',
+  color: 'blue',
+  price: 150.00,
   yop: 1940)
